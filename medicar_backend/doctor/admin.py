1276c730755c1ee2c983733434388eb767047ec3
Mario <mario.nascimento@aluno.uece.br>
2020-03-20 20:34:55 -0300

    Adds apps to admin site

--- medicar_backend/doctor/admin.py
+++ medicar_backend/doctor/admin.py
@@ -1,3 +1,5 @@
+from .models import Doctor, Specialty
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Specialty)
+admin.site.register(Doctor)
--- medicar_backend/schedule/admin.py
+++ medicar_backend/schedule/admin.py
@@ -1,3 +1,5 @@
+from .models import AppointmentTime, Schedule
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(AppointmentTime)
+admin.site.register(Schedule)
