847c71f48df5b3966534a36e8ff703bb2bcd21dd
Mario <mario.nascimento@aluno.uece.br>
2020-03-20 19:14:53 -0300

    Creates Doctor and Specialty models

---
+++ medicar_backend/doctor/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Specialty(models.Model):
+    """Specialty Model"""
+
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'specialties'
+
+
+class Doctor(models.Model):
+    """Doctor Model"""
+
+    nome = models.CharField(max_length=100)
+    crm = models.CharField(max_length=30)
+    email = models.EmailField(null=True, blank=True)
+    telefone = models.CharField(max_length=30, null=True, blank=True)
+    especialidade = models.ForeignKey(
+        Specialty, on_delete=models.CASCADE, null=True, blank=True)
+
+    def minha_especialidade(self):
+        return self.especialidade.nome
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'doctors'
