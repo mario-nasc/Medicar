782289610aca7df0ae29dda3d6d5f1cdf730b2f8
Mario <mario.nascimento@aluno.uece.br>
2020-03-21 09:35:28 -0300

    Improve: Django Admin organization

--- medicar_backend/doctor/admin.py
+++ medicar_backend/doctor/admin.py
@@ -1,5 +1,21 @@
+from django.contrib.auth.models import Group
 from .models import Doctor, Specialty
 from django.contrib import admin
 
-admin.site.register(Specialty)
-admin.site.register(Doctor)
+admin.site.site_header = 'Medicar - Gerência'
+
+
+class SpecialtyAdmin(admin.ModelAdmin):
+    """Doctor Admin"""
+    list_display = ('nome',)
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    """Doctor Admin"""
+    list_display = ('nome', 'crm', 'minha_especialidade')
+    list_filter = ('nome', 'crm')
+
+
+admin.site.register(Specialty, SpecialtyAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.unregister(Group)
